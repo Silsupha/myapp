e5b9465dc54048ee0bf6cccd232b54cd9403f123
Silsupha <102601136+Silsupha@users.noreply.github.com>
2023-12-11 00:10:21 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import openai
 import json
@@ -9,6 +10,17 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 # Initialize the OpenAI GPT-3 client
 openai.api_key = user_api_key
 
+prompt = """Act as an AI writing tutor in English. You will receive a 
+            piece of writing and you should give suggestions on how to improve it.
+            List the suggestions in a JSON array, one suggestion per line.
+            Each suggestion should have 3 fields:
+            - "before" - the text before the suggestion
+            - "after" - the text after the suggestion
+            - "category" - the category of the suggestion one of "grammar", "style", "word choice", "other"
+            - "comment" - a comment about the suggestion
+            Don't say anything at first. Wait for the user to say something.
+        """    
+
 # Streamlit app
 st.title("Historical Events Summarizer")
 st.markdown("Enter a piece of text, and the app will summarize the events that occurred in different years.")
@@ -19,17 +31,20 @@ user_input = st.text_area("Enter the article or text:", "")
 # Submit button
 if st.button("Summarize Events"):
     # Create a prompt using user input
-    prompt = f"Summarize the events that occurred in different years based on the following text:\n\n{user_input}"
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
+    ]
 
     # Generate response from GPT-3 model
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=prompt,
+        messages=messages_so_far,
         max_tokens=400
     )
 
     # Extract summarized events from the generated response
-    summarized_events = response.choices[0].text.strip()
+    summarized_events = response.choices[0].message.content
 
     # Parse the summarized events into a DataFrame
     events_list = [event.split(': ') for event in summarized_events.split('\n')]
