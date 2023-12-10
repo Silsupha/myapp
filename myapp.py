b95db7da8d32cd06f9b8e03ac87f6661e48ebbaf
Silsupha <102601136+Silsupha@users.noreply.github.com>
2023-12-10 22:31:49 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,67 +1,47 @@
 import streamlit as st
-import pandas as pd
 import openai
-import string
+import pandas as pd
 
-# เติม OpenAI API key ผ่าน sidebar
+# Set your OpenAI API key via the Streamlit sidebar
 openai.api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
 
-# ฟังก์ชันสำหรับสรุปบทความ
-def summarize_article(article):
-    prompt = f"Summarize the following article:\n{article}"
-    response = openai.Completion.create(
+# Function to summarize text using ChatGPT
+def summarize_text(input_text):
+    prompt = f"Summarize the following text:\n{input_text}"
+    response = openai.chat.completions.create(
         model="text-davinci-002",
         messages=prompt,
         max_tokens=150
     )
     return response.choices[0].text.strip()
 
-# ฟังก์ชันสำหรับดึงคำศัพท์ยากและ synonym
-def extract_difficult_words(article):
-    words = article.translate(str.maketrans("", "", string.punctuation)).split()
-
-    difficult_words = []
-    synonyms = []
-
-    for word in words:
-        if len(word) > 5 and word.isalpha() and word not in difficult_words:
-            difficult_words.append(word)
-            # For simplicity, we're not including synonyms without additional packages
-
-    return difficult_words[:10], synonyms[:10]
-
-# หน้าหลักของ Streamlit application
+# Streamlit app
 def main():
-    st.title("Article Summarizer and Difficult Words Extractor")
+    st.title("Text Summarization with ChatGPT")
 
-    # รับ input จากผู้ใช้
-    article = st.text_input
+    # User input: text area to input the text
+    input_text = st.text_area("Enter the text you want to summarize:")
 
-    # กรณีมีข้อความใน input
-    if article:
-        # สรุปบทความ
-        summary = summarize_article(article)
-        st.subheader("Article Summary:")
-        st.write(summary)
+    # Process the input and generate a summary
+    if st.button("Generate Summary"):
+        if input_text:
+            summary = summarize_text(input_text)
 
-        # ดึงคำศัพท์ยากและ synonym
-        difficult_words, synonyms = extract_difficult_words(article)
+            # Display the summary
+            st.subheader("Summary:")
+            st.write(summary)
 
-        # แสดงผลลัพธ์ในรูปแบบของ pandas dataframe
-        df_result = pd.DataFrame({"Difficult Words": difficult_words, "Synonyms": synonyms})
-        st.subheader("Difficult Words and Synonyms:")
-        st.dataframe(df_result, index=False)
+            # Create a DataFrame to download the summary as CSV
+            df_result = pd.DataFrame({"Original Text": [input_text], "Summary": [summary]})
+            st.markdown(get_table_download_link(df_result), unsafe_allow_html=True)
 
-        # สร้างลิงก์สำหรับดาวน์โหลดผลลัพธ์เป็นไฟล์ CSV
-        csv_link = create_download_link(df_result, "Download Difficult Words and Synonyms")
-        st.markdown(csv_link, unsafe_allow_html=True)
-
-# ฟังก์ชันสร้างลิงก์ดาวน์โหลดไฟล์ CSV
-def create_download_link(df, text):
+# Function to create a download link for a DataFrame
+def get_table_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="difficult_words_synonyms.csv">{text}</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="summary.csv">Download Summary</a>'
     return href
 
 if __name__ == "__main__":
     main()
+
