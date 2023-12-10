441764fabc3769039a6e895495799b2096c3cd47
Silsupha <102601136+Silsupha@users.noreply.github.com>
2023-12-10 23:47:14 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,47 +1,37 @@
 import streamlit as st
 import openai
+import json
 import pandas as pd
 
-# Set your OpenAI API key via the Streamlit sidebar
-openai.api_key = st.sidebar.text_input("Enter your OpenAI API key", type="password")
+# Get the API key from the sidebar called OpenAI API key
+user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
-# Function to summarize text using ChatGPT
-def summarize_text(input_text):
-    prompt = f"Summarize the following text:\n{input_text}"
-    response = openai.chat.completions.create(
-        model="text-davinci-002",
-        messages=prompt,
-        max_tokens=150
+client = openai.OpenAI(api_key=user_api_key)
+prompt = """Transform the following informal sentences into formal language and remove any unnecessary sentences:
+"""
+
+st.title('Formalization and Sentence Filtering App')
+st.markdown('Input a list of informal sentences. The AI will transform them into formal language and filter out unnecessary sentences.')
+
+user_input = st.text_area("Enter informal sentences (one per line):", "Your sentences here")
+
+# submit button after text input
+if st.button('Transform and Filter'):
+    sentences = user_input.split('\n')
+    messages_so_far = [
+        {"role": "system", "content": prompt},
+        {'role': 'user', 'content': user_input},
+    ]
+    response = client.chat.completions.create(
+        model="text-davinci-003",
+        messages=messages_so_far
     )
-    return response.choices[0].text.strip()
+    # Show the response from the AI in a box
+    st.markdown('**AI response:**')
+    suggestion_dictionary = response.choices[0].message.content
 
-# Streamlit app
-def main():
-    st.title("Text Summarization with ChatGPT")
-
-    # User input: text area to input the text
-    input_text = st.text_area("Enter the text you want to summarize:")
-
-    # Process the input and generate a summary
-    if st.button("Generate Summary"):
-        if input_text:
-            summary = summarize_text(input_text)
-
-            # Display the summary
-            st.subheader("Summary:")
-            st.write(summary)
-
-            # Create a DataFrame to download the summary as CSV
-            df_result = pd.DataFrame({"Original Text": [input_text], "Summary": [summary]})
-            st.markdown(get_table_download_link(df_result), unsafe_allow_html=True)
-
-# Function to create a download link for a DataFrame
-def get_table_download_link(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="summary.csv">Download Summary</a>'
-    return href
-
-if __name__ == "__main__":
-    main()
-
+    sd = json.loads(suggestion_dictionary)
+    suggestion_df = pd.DataFrame.from_dict(sd)
+    
+    # Display the transformed and filtered result
+    st.table(suggestion_df)
