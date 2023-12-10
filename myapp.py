c59161dac8948605476ba66297a73affd0f4716a
Silsupha <102601136+Silsupha@users.noreply.github.com>
2023-12-10 19:30:42 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -18,30 +18,30 @@ st.markdown('Input group of words that you want to write poem about. \n\
 user_input = st.text_area("Enter word group : ", "Your words here")
 
 def generate_poem(prompt_text):
-    response = client.chat.completions.create(
-        engine="text-davinci-002",  # Specify the GPT-3.5-turbo engine
-        prompt=prompt_text,
-        max_tokens=150,  # Adjust max_tokens as needed
-        temperature=0.7,  # Adjust temperature as needed
-    )
+            response = client.chat.completions.create(
+                        engine="text-davinci-002",  # Specify the GPT-3.5-turbo engine
+                        prompt=prompt_text,
+                        max_tokens=150,  # Adjust max_tokens as needed
+                        temperature=0.7,  # Adjust temperature as needed)
     return response.choices[0].text.strip()
 
 if st.button('Submit'):
-    if user_input:
-        # Call the function to generate poem
-        poem_result = generate_poem(user_input)
+            if user_input:
+            # Call the function to generate poem
 
-        # Display results using Pandas DataFrame
-        result_df = pd.DataFrame({"Generated Poem": [poem_result]})
-        st.dataframe(result_df)
+                        poem_result = generate_poem(user_input)
 
-        # Provide Download Link for Results as CSV
-        csv_data = result_df.to_csv(index=False).encode()
-        st.download_button(
-            label="Download Poem as CSV",
-            data=csv_data,
-            file_name="generated_poem.csv",
-            key="download_csv",
-        )
-    else:
-        st.warning("Please enter a group of words for the poem.")
+                        # Display results using Pandas DataFrame
+                        result_df = pd.DataFrame({"Generated Poem": [poem_result]})
+                        st.dataframe(result_df)
+
+                        # Provide Download Link for Results as CSV
+                        csv_data = result_df.to_csv(index=False).encode()
+                        st.download_button(
+                                    label="Download Poem as CSV",
+                                    data=csv_data,
+                                    file_name="generated_poem.csv",
+                                    key="download_csv",
+            )
+            else:
+                        st.warning("Please enter a group of words for the poem.")
