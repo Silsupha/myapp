9f3b4c55a64c22c277e01972ea85dd7e93a93fe1
Silsupha <102601136+Silsupha@users.noreply.github.com>
2023-12-11 16:55:25 +0700

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,48 +2,35 @@ import streamlit as st
 import openai
 import pandas as pd
 
-# Set OpenAI API key
+# Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
 
-# Streamlit app
 st.title('Multiple-Choice Question Generator')
-st.markdown('Input a passage, and the app will generate multiple-choice questions for you.')
+st.markdown('Input a passage, and the app will generate multiple-choice (4 choices) questions for you.')
 
-# User input for the passage
 passage_input = st.text_area("Enter a passage:", "Your passage here")
 
-# submit button after passage input
+# submit button after text input
 if st.button('Generate Multiple-Choice Questions'):
-    # Create a message array with system message and user message
     messages = [
         {"role": "system", "content": "Generate multiple-choice questions for the given passage:"},
         {"role": "user", "content": passage_input},
     ]
-
-    # Request multiple-choice questions from OpenAI
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
         temperature=0.5,
         max_tokens=200
     )
-
-    # Extract multiple-choice questions, correct answers, and explanations from the response
+    
     mcq_data = response.choices[0].message.content.split('\n')
 
-    # Create a list to store rows
     rows = []
-
-    # Populate the list
     for i in range(0, len(mcq_data), 6):
         question = mcq_data[i].split('.')[1].strip()  # Remove the question number
         choices = mcq_data[i + 1:i + 5]
-
-        # Extract the correct answer by checking for "(Correct)" in each choice
         correct_answer = next((choice.replace("(Correct)", "").strip() for choice in choices if "(Correct)" in choice), "")
-
-        # Skip the question if it doesn't have enough meaningful choices
         if len(choices) == 4:
             rows.append({
                 'Question': question,
@@ -53,10 +40,7 @@ if st.button('Generate Multiple-Choice Questions'):
                 'Choice D': choices[3],
                 'Correct Answer': correct_answer
             })
-
-    # Create the dataframe
     result_df = pd.DataFrame(rows)
 
-    # Display the resulting dataframe
     st.subheader('Generated Multiple-Choice Questions:')
     st.table(result_df)
